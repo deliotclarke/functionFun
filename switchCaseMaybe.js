783c88b0108e0b920d8f69e255dc6ffe317b5f2e
Eliot Clarke <45045397+deliotclarke@users.noreply.github.com>
2019-01-08 17:10:15 -0600

    Create switchCaseMaybe.js
    just an idea to play with

---
+++ switchCaseMaybe.js
@@ -0,0 +1,9 @@
+console.log("We have 3 cups and 1 ball. Your job will be to guess which cup the ball is in using the numbers 1, 2 or 3.")
+var userInput1 = prompt ("Are you ready??", "Y or N?")
+switch (userInput1) {
+    case "Y";
+    case "N";
+    default
+}
+
+//just an example
