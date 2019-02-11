0941856ae23c07c4aae50cec95077bc59d6f72e2
Eliot Clarke <45045397+deliotclarke@users.noreply.github.com>
2019-02-11 10:59:29 -0600

    cleaned up some of the functions
    cleaned up functions, changed some code to work better with less variables, fixed console.logs - still may change to string interpolation, back tic stuff. added hiddenBall var to store the random value from arrayCups - double checked it with console.logs on codepen and it totally works!! gonna create a new branch to show progress. 
    
    STILL NEEDS: cleaned up, want to throw it into a code editor to check for missing stuff. I need to decide whether or not i'm going to use ;'s, this random happening stuff is no good. need to figure out prompts. last function needs fallbacks for null answers. should i turn it all into a loop? probablyyyyyyyyyy

--- originCode.js
+++ originCode.js
@@ -1,9 +1,3 @@
-var arrayCups = [cup1, cup2, cup3];
-
-var hiddenBall = arrayCups[Math.floor(Math.random()*arrayCups.length)]; 
-//to store the randomized numbers for the user to guess?
-
-
 console.log("We have 3 cups and 1 ball. Your job will be to guess which cup the ball is in using the numbers 1, 2 or 3.")
 var userInput1 = prompt ("Are you ready??", "Y or N?")
 //prompt for user to input Y or N
@@ -20,36 +14,36 @@ if (userInput1 == answerY){
 //now user prompt to function userGuess 
 } else if {(userInput1 == answerN){
   console.log("Then why are you here? Actually... Why are any of us here? What are we doing? What balls?... In the grand scheme of space and time what balls even matter? Should we stay or should we go now, amiright? Stay or Go?")
-} else if {(userInput1 == answerStay)
+  //open up new user prompt
+} else if {(userInput1 == answerStay){
   console.log("ALRIGHT! Let's do this!")
   return answerY;
   //not sure if this would work
-}  
   //return to guess
-  
-if (typeof answerGo){
+} else if {(userInput1 == answerGo){
   console.log("It's fine. I get it. I'll be okay. It only took me like... way too long to make this, but yeah, enjoy instagram.")
-}
-
-if (typeof userWhatever){
-  console.log(userWhatever += " does not compute, my dude. Maybe refresh or kick rocks?")
-//for the obstinate user; no more prompts
+} else {(typeof userWhatever){
+  console.log(userWhatever += " does not compute, my dude. Maybe refresh or kick rocks?")}
+        }
+      }
+    }
+  }
+};
 
 //try out a function?
-funtion userGuess() {
-  var userInput = "";
-  var outputCorrect;
-  var outputIncorrect;
-  var outputFinal;
-  var correctAnswer;
+function userGuess() {
+var arrayCups = ["Cup 1", "Cup 2", "Cup 3"];
+var hiddenBall = arrayCups[Math.floor(Math.random()*arrayCups.length)]; 
+var userInput2 = "";
+var outputFinal;
   
   //prompt for user input goes here
   
-  if (typeof userInput == correctAnswer){
-  outputCorrect += " is correct!! You win!")
-  if (typeof userInput != incorrectAnswer){
-  outputIncorrect += " is noooot correct. Sorry bud. Try Again?"
+if (userInput2 == hiddenBall){
+  console.log(userInput2 += " is correct!! You win!");
+} else if {(userInput2 != hiddenBall){
+  console.log(userInput2 += " is noooot correct. Sorry bud. Try Again?");
   }
-  }
+};
   
 // ^eventually turn into a user prompt
